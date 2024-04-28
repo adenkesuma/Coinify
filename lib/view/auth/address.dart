1f99d09849ad3c0d21a464712534c253d77d8797
0b1111001 <anderusman2004@gmail.com>
2024-04-28 09:35:11 +0700

    Overflow ketika dicollapse, sedang di fix

--- lib/view/auth/address.dart
+++ lib/view/auth/address.dart
@@ -52,12 +52,12 @@ class _AddressState extends State<Address> {
           ),
         )
       ),
-      body: _open ? SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Container(
           // padding: EdgeInsets.symmetric(horizontal: 24.0,vertical: 12.0),
           padding: EdgeInsets.all(30.0),
           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height-1/9*(MediaQuery.of(context).size.height),
+          height: _open ? MediaQuery.of(context).size.height-1/9*(MediaQuery.of(context).size.height)+100 : MediaQuery.of(context).size.height-1/9*(MediaQuery.of(context).size.height),
           // height: MediaQuery.of(context).size.height,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -93,77 +93,75 @@ class _AddressState extends State<Address> {
                     ),
                   ),
                   SizedBox(height: 40,),
-                  SingleChildScrollView(
-                    child: Theme(
-                      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-                      child: ListTileTheme(
-                        contentPadding: EdgeInsets.all(0),
-                        dense: true,
-                        child: ExpansionTile(
-                          title: Text("Enter Address Manually",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                          onExpansionChanged: (bool val) {
-                            setState(() {
-                              _open = val;
-                            });
-                          },
-                          children: [
-                            Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(height: 8.0,),
-                                  Text("Address line 1",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
+                  Theme(
+                    data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+                    child: ListTileTheme(
+                      contentPadding: EdgeInsets.all(0),
+                      dense: true,
+                      child: ExpansionTile(
+                        title: Text("Enter Address Manually",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
+                        onExpansionChanged: (bool value) {
+                          setState(() {
+                            _open = value;
+                          });
+                        },
+                        children: [
+                          Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 8.0,),
+                                Text("Address line 1",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.grey)
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.blue),
+                                    )
+                                  ),
+                                ),
+                                SizedBox(height: 8.0,),
+                                Text("Address line 2 (optional)",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
+                                TextFormField(
+                                  decoration: InputDecoration(
                                       enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(color: Colors.grey)
+                                          borderSide: BorderSide(color: Colors.grey)
                                       ),
                                       focusedBorder: OutlineInputBorder(
                                         borderSide: BorderSide(color: Colors.blue),
                                       )
-                                    ),
                                   ),
-                                  SizedBox(height: 8.0,),
-                                  Text("Address line 2 (optional)",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.grey)
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(color: Colors.blue),
-                                        )
-                                    ),
+                                ),
+                                SizedBox(height: 8.0,),
+                                Text("City",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.grey)
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.blue),
+                                      )
                                   ),
-                                  SizedBox(height: 8.0,),
-                                  Text("City",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.grey)
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(color: Colors.blue),
-                                        )
-                                    ),
+                                ),
+                                SizedBox(height: 8.0,),
+                                Text("City",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.grey)
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.blue),
+                                      )
                                   ),
-                                  SizedBox(height: 8.0,),
-                                  Text("City",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.grey)
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(color: Colors.blue),
-                                        )
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
                       ),
                     ),
                   )
@@ -197,153 +195,7 @@ class _AddressState extends State<Address> {
             ],
           ),
         ),
-      ) :
-      Expanded(
-        child: Container(
-          // padding: EdgeInsets.symmetric(horizontal: 24.0,vertical: 12.0),
-          padding: EdgeInsets.all(30.0),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height-1/9*(MediaQuery.of(context).size.height),
-          // height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Enter your address",style: TextStyle(fontSize: 22,fontFamily: "GraphikMedium"),),
-                  SizedBox(height: 12,),
-                  Text("Enter the street address of your primary residence. Please do not use a PO box or business address",
-                    style: TextStyle(color: Colors.grey,fontSize: 16),),
-                  SizedBox(height: 24,),
-                  Text("Search for Address",style: TextStyle(fontSize: 16.0,fontFamily: "GraphikMedium",
-                      color: addressText.hasFocus ? Colors.blue : Colors.black),
-                  ),
-                  SizedBox(height: 8.0,),
-                  Focus(
-                    onFocusChange: (focus){
-                      setState(() {});
-                    },
-                    child: TextFormField(
-                      focusNode: addressText,
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue)
-                          ),
-                          hintText: "Enter your address",
-                          hintStyle: TextStyle(color: Colors.grey)
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 40,),
-                  SingleChildScrollView(
-                    child: Theme(
-                      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-                      child: ListTileTheme(
-                        contentPadding: EdgeInsets.all(0),
-                        dense: true,
-                        child: ExpansionTile(
-                          title: Text("Enter Address Manually",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                          onExpansionChanged: (bool val) {
-                            setState(() {
-                              _open = val;
-                            });
-                          },
-                          children: [
-                            Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(height: 8.0,),
-                                  Text("Address line 1",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.grey)
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(color: Colors.blue),
-                                        )
-                                    ),
-                                  ),
-                                  SizedBox(height: 8.0,),
-                                  Text("Address line 2 (optional)",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.grey)
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(color: Colors.blue),
-                                        )
-                                    ),
-                                  ),
-                                  SizedBox(height: 8.0,),
-                                  Text("City",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.grey)
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(color: Colors.blue),
-                                        )
-                                    ),
-                                  ),
-                                  SizedBox(height: 8.0,),
-                                  Text("City",style: TextStyle(fontFamily: "GraphikRegular",fontSize: 16.0),),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.grey)
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(color: Colors.blue),
-                                        )
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                  onPressed: (){
-                    Navigator.pushNamed(context, '/user-purpose');
-                  },
-                  child: Text(
-                    "Continue",
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white,
-                      fontFamily: "GraphikMedium",
-                    ),
-                  ),
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0))
-                      ),
-                      minimumSize: MaterialStateProperty.all(Size.fromHeight(60.0)),
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
-                      shadowColor: MaterialStateProperty.all(Colors.transparent)
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      )
     );
   }
 }
