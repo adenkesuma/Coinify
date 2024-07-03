2e13fc9c741951a0ab81f08644ed0c9ee0de9929
Darmin Susanto <221111398@students.mirkoskil.ac.id>
2024-07-03 19:29:13 +0700

    Membuat halaman share address utk receive bitcoin

---
+++ lib/view/share-address.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+class ShareAddress extends StatefulWidget {
+  const ShareAddress({super.key});
+
+  @override
+  State<ShareAddress> createState() => _ShareAddressState();
+}
+
+class _ShareAddressState extends State<ShareAddress> {
+  String address = "0z890085...2e2a80Ea5";
+
+   void copyToClipboard(String text) {
+    Clipboard.setData(ClipboardData(text: text));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Copied to clipboard')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Receive Bitcoin"),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: 500,
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 50),
+                        child: Image.asset("assets/Qrcode_wikipedia.jpg", 
+                          width: 300, height: 300)
+                        ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        height: 1,
+                        color: Colors.grey,
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Wallet address",
+                              style: const TextStyle(
+                                  fontSize: 16,
+                                ),),
+                              Text(address,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: SizedBox(
+                                width: 100,
+                                height: 50,
+                                child: TextButton(
+                                  onPressed: () {
+                                  copyToClipboard(address);
+                                  },
+                                  
+                                  style: TextButton.styleFrom(
+                                    padding: EdgeInsets.zero,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      side: BorderSide(color: Colors.grey),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "Copy",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 200,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 60,
+                    child: TextButton(
+                      onPressed: (){}, 
+                      child: Text(
+                        "Share address",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: BorderSide(color: Colors.transparent),
+                        ),
+                        backgroundColor: Color.fromARGB(255, 2, 29, 235),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
